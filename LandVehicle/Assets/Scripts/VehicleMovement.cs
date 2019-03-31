eff449c63efee2d993ea2123c0fa18d801b4a615
Mike Morrone <Hanegwen725@gmail.com>
2019-03-31 09:38:12 -0500

    added stuff

---
+++ LandVehicle/Assets/Scripts/VehicleMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleMovement : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    bool CanJump = true;
+    float JumpCooldownTime = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ActivateJump()
+    {
+        CanJump = false;
+        StartCoroutine(JumpCooldown());
+    }
+
+    IEnumerator JumpCooldown()
+    {
+        yield return new WaitForSeconds(JumpCooldownTime);
+        CanJump = true;
+
+    }
+}
