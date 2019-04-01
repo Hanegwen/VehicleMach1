c9005aae10b72e379f0252b14e838ef567f5ea78
Mike Morrone <Hanegwen725@gmail.com>
2019-04-01 16:38:47 -0500

    Tests

--- LandVehicle/Assets/Scripts/VehicleMovement.cs
+++ LandVehicle/Assets/Scripts/VehicleMovement.cs
@@ -8,6 +8,18 @@ public class VehicleMovement : MonoBehaviour
 
     bool CanJump = true;
     float JumpCooldownTime = 3f;
+    float currentFuel = 100;
+    float maxFuel = 100;
+    float fuelRechargeRate = 1;
+    float jumpSeed = 1;
+
+    float maxSpeed = 100;
+    float currentSpeed = 0;
+    float accelerationSpeed = 1;
+
+    float turnSpeed = 50;
+
+    bool EngineIsOn = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,19 +29,112 @@ public class VehicleMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            EngineIsOn = true;
+        }
+        else
+        {
+            EngineIsOn = false;
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            MoveForwards();
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            MoveBackwards();
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            TurnRight();
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            TurnLeft();
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            ActivateJump();
+        }
+
+        else
+        {
+            if(currentFuel + fuelRechargeRate < maxFuel)
+            {
+                currentFuel += fuelRechargeRate;
+            }
+        }
+
+        AdjustCurrentSpeed();
+    }
+
+    void AdjustCurrentSpeed()
+    {
+        if (currentSpeed >= 0)
+        {
+            if(EngineIsOn)
+            {
+                if(currentSpeed + accelerationSpeed < maxSpeed)
+                {
+                    currentSpeed += accelerationSpeed;
+                }
+            }
+            else
+            {
+                currentSpeed -= accelerationSpeed;
+            }
+        }
+        else if(currentSpeed < 0)
+        {
+            currentSpeed = 0;
+        }
+    }
+
+    void TurnRight()
+    {
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * -1);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * -1);
+    }
+
+    void TurnLeft()
+    {
+        transform.Rotate(Vector3.down, turnSpeed * Time.deltaTime * -1);
+        // transform.Translate(Vector3.left * Time.deltaTime * turnSpeed * -1);
+    }
+
+    void MoveBackwards()
+    {
+        transform.Translate(Vector3.back * Time.deltaTime * currentSpeed);
+        //rigidbody.AddForce((Vector3.back * maxSpeed),ForceMode.Acceleration);
+    }
+
+    void MoveForwards()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed);
+        //rigidbody.velocity = Vector3.forward * maxSpeed;
     }
 
     void ActivateJump()
     {
-        CanJump = false;
-        StartCoroutine(JumpCooldown());
+        if(currentFuel > 0)
+        {
+            this.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y + jumpSeed, this.gameObject.transform.position.z);
+
+            currentFuel -= fuelRechargeRate;
+        }
     }
 
     IEnumerator JumpCooldown()
     {
         yield return new WaitForSeconds(JumpCooldownTime);
-        CanJump = true;
+        
 
     }
 }
